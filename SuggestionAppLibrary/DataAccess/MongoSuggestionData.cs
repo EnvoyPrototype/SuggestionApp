72695189f9c9f1ae403a4de552e7fc6e8dd6367a
Jim Rogers <hello@jimrogers.dev>
2022-10-20 13:53:24 -0500

    Fix db error in MongoSuggestionData.cs

--- SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
+++ SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
@@ -94,10 +94,11 @@ public class MongoSuggestionData : ISuggestionData
 				suggestion.UserVotes.Remove(userId);
 			}
 
-			await suggestionsInTransaction.ReplaceOneAsync(s => s.Id == suggestionId, suggestion);
+			// Add session to be able to roll back transaction
+			await suggestionsInTransaction.ReplaceOneAsync(session, s => s.Id == suggestionId, suggestion);
 
 			var usersInTransaction = db.GetCollection<UserModel>(_db.UserCollectionName);
-			var user = await _userData.GetUser(suggestion.Author.Id);
+			var user = await _userData.GetUser(userId);
 
 			if (isUpvote)
 			{
@@ -108,7 +109,7 @@ public class MongoSuggestionData : ISuggestionData
 				var suggestionToRemove = user.VotedOnSuggestions.Where(s => s.Id == suggestionId).First();
 				user.VotedOnSuggestions.Remove(new BasicSuggestionModel(suggestion));
 			}
-			await usersInTransaction.ReplaceOneAsync(u => u.Id == userId, user);
+			await usersInTransaction.ReplaceOneAsync(session, u => u.Id == userId, user);
 
 			await session.CommitTransactionAsync();
 
@@ -135,12 +136,12 @@ public class MongoSuggestionData : ISuggestionData
 		{
 			var db = client.GetDatabase(_db.DbName);
 			var suggestionsInTransaction = db.GetCollection<SuggestionModel>(_db.SuggestionCollectionName);
-			await suggestionsInTransaction.InsertOneAsync(suggestion);
+			await suggestionsInTransaction.InsertOneAsync(session, suggestion);
 
 			var usersInTransaction = db.GetCollection<UserModel>(_db.UserCollectionName);
 			var user = await _userData.GetUser(suggestion.Author.Id);
 			user.AuthoredSuggestions.Add(new BasicSuggestionModel(suggestion));
-			await usersInTransaction.ReplaceOneAsync(u => u.Id == user.Id, user);
+			await usersInTransaction.ReplaceOneAsync(session, u => u.Id == user.Id, user);
 
 			await session.CommitTransactionAsync();
 		}
